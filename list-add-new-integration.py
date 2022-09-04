1d4bbf9d6c1ee40f7d437bb23802b2c2637fa85b
MiklosCsecsi_Snyk <105910607+mcsnyk@users.noreply.github.com>
2022-09-04 19:51:31 +0200

    add: list-add-new-integration.py

---
+++ list-add-new-integration.py
@@ -0,0 +1,62 @@
+import httpx
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def create_client(base_url: str, token: str) -> httpx.Client:
+    """Create a client"""
+
+    client = httpx.Client(
+        base_url=base_url,
+        headers={
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": f"token {token}",
+        },
+    )
+    return client
+
+
+def list_existing_integrations(client: httpx.Client, org_id: str) -> httpx.Response:
+    """List all existing integrations for an org"""
+
+    response = client.get(f"/org/{org_id}/integrations")
+    return response.json()
+
+
+def add_new_integration(client: httpx.Client, org_id: str, integr_type: str,  integr_token: str, url: str) -> httpx.Response:
+    """Adding a new integration to an existing org"""
+
+    new_integration_data = {
+        "type": integr_type,
+        "credentials": {
+            "token": integr_token,
+            "url": url
+        }
+    }
+
+    response = client.post(f"/org/{org_id}/integrations", data=json.dumps(new_integration_data))
+    return response.json()
+
+
+def main():
+    """Main function. """
+    SNYK_TOKEN = "bf9b9173-fdf6-4d2a-b603-ebc6e6b0f341"
+    ORG_ID = "b6553a7e-9051-43aa-8bf7-3437e1918e04"
+    INTEGR_TYPE = "gitlab"
+    INTEGR_TOKEN = "glpat-9YciuBMPA3sqSdfuzNRv"
+    URL = "https://gitlab.com"
+
+    client = create_client(base_url="https://api.snyk.io/api/v1", token=SNYK_TOKEN)
+
+    list_integrations = list_existing_integrations(client, org_id=ORG_ID)
+    print(json.dumps(list_integrations, indent=2))
+
+    #new_integration = add_new_integration(client, org_id=ORG_ID, integr_type=INTEGR_TYPE, integr_token=INTEGR_TOKEN, url=URL)
+    #print(json.dumps(new_integration, indent=2))
+
+
+if __name__ == '__main__':
+    main()
+
