56aa81b4f1654803200d378a875109f1e4104f26
MiklosCsecsi_Snyk <105910607+mcsnyk@users.noreply.github.com>
2022-09-06 08:59:42 +0200

    fix: credentials

--- move-projects.py
+++ move-projects.py
@@ -35,16 +35,20 @@ def moving_orgs(client: httpx.Client, org_id: str, proj_id: str, target_org_id: 
 
 def main():
     """Main function"""
-    SNYK_TOKEN = "bf9b9173-fdf6-4d2a-b603-ebc6e6b0f341"
-    PROJECT_ID = "a3aef1b0-0be5-4c58-aa9b-31289f08ae1d"
-    ORIGIN_ORG_ID = "dfe66b48-2bf2-4396-b77a-dfdb980e2cf8"
-    TARGET_ORG_ID = "b6553a7e-9051-43aa-8bf7-3437e1918e04"
-
+    
+    SNYK_TOKEN = "..."
+    PROJECT_ID = "..."
+    ORIGIN_ORG_ID = "..."
+    TARGET_ORG_ID = "..."
+    
+    # create a client:
     client = create_client(base_url="https://api.snyk.io/api/v1", token=SNYK_TOKEN)
-
+    
+    # moving project from one org into another
     moving_org = moving_orgs(client, org_id=ORIGIN_ORG_ID, proj_id=PROJECT_ID, target_org_id=TARGET_ORG_ID)
-    print(moving_org)
+    logging.debug(json.dumps(moving_org, indent=2))
 
 
 if __name__ == '__main__':
     main()
+   
